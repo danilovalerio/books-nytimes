d8b3aa379a894af2073e9da2242abff83c3fad33
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-26 18:21:44 -0300

    Toolbar personalizada e RecyclerView

--- app/src/main/java/com/example/booksnytimes/books/BooksActivity.kt
+++ app/src/main/java/com/example/booksnytimes/books/BooksActivity.kt
@@ -3,11 +3,16 @@ package com.example.booksnytimes.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.booksnytimes.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Livros"
+        setActionBar(toolbarMain)
+
     }
 }

f58020102b33b138487228fc54f0806761d3a270
Danilo Valerio Silva <dvalerio@everis.com>
2019-09-26 19:11:05 -0300

    BooksViewModel criada

---
+++ app/src/main/java/com/example/booksnytimes/presentation/books/BooksViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.booksnytimes.presentation.books
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * View model tem que ser independente do seu Android
+ * Totalmente isolada da activity e do fragment
+ */
+
+class BooksViewModel : ViewModel() {
+
+}
